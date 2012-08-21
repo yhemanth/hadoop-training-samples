e49b317b5959205f70d809380361718bcf33f42b
Hemanth Yamijala <hemanty@Hemanths-MacBook-Pro.local>
2012-08-21 20:05:19 +0530

    Added Status listing

---
+++ src/FsStatusLister.java
@@ -0,0 +1,25 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.io.IOException;
+
+public class FsStatusLister {
+    public static void main(String[] args) throws IOException {
+        if (args.length < 1) {
+            System.out.println("Usage: hadoop FsStatusLister <path>");
+        }
+        FileSystem fileSystem = FileSystem.get(new Configuration());
+        FileStatus[] fileStatuses = fileSystem.globStatus(new Path(args[0]));
+        if (fileStatuses == null) {
+            System.out.println("No files found");
+            return;
+        }
+        for (FileStatus status : fileStatuses) {
+            String type = status.isDir() ? "Directory" : "File";
+            System.out.println(String.format("%s %s %.2fKb %d", status.getPath().getName(), type, status.getLen()/1024.0,
+                    status.getReplication()));
+        }
+    }
+}
