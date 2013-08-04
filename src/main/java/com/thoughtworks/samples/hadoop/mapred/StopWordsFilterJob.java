47fdbeb508e96b7d3397fc4c6fd86c754c870fb2
Hemanth Yamijala <hemanty@thoughtworks.com>
2013-08-04 23:18:21 +0530

    Modified code to take the cache file as argument.

--- src/main/java/com/thoughtworks/samples/hadoop/mapred/StopWordsFilterJob.java
+++ src/main/java/com/thoughtworks/samples/hadoop/mapred/StopWordsFilterJob.java
@@ -2,7 +2,6 @@ package com.thoughtworks.samples.hadoop.mapred;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.filecache.DistributedCache;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -12,14 +11,11 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
-import java.net.URI;
-
 public class StopWordsFilterJob extends Configured implements Tool {
     @Override
     public int run(String[] args) throws Exception {
 
-        Configuration conf = new Configuration();
-        DistributedCache.addCacheFile(new URI("hdfs://localhost:9000/user/hemanty/stop_words.txt"), conf);
+        Configuration conf = getConf();
 
         Job stopWordsFilterJob = new Job(conf, "StopWordsFilter");
         stopWordsFilterJob.setJarByClass(StopWordsFilterJob.class);
