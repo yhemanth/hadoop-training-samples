ed6e3dbc3b0f96b2f046305a7fcb6357d421170e
Hemanth Yamijala <hemanty@thoughtworks.com>
2014-01-09 08:13:40 +0530

    Modified way in which combiner should be specified - through configuration

--- src/main/java/com/thoughtworks/samples/hadoop/mapred/wordcount/MyWordCount.java
+++ src/main/java/com/thoughtworks/samples/hadoop/mapred/wordcount/MyWordCount.java
@@ -1,23 +1,29 @@
 package com.thoughtworks.samples.hadoop.mapred.wordcount;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
-import java.io.IOException;
+public class MyWordCount extends Configured implements Tool {
 
-public class MyWordCount {
+    public static void main(String[] args) throws Exception {
+        ToolRunner.run(new Configuration(), new MyWordCount(), args);
+    }
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
-        Configuration conf = new Configuration();
+    @Override
+    public int run(String[] args) throws Exception {
+        Configuration conf = getConf();
         Job wordCountJob = new Job(conf, "MyWordCount");
         wordCountJob.setJarByClass(MyWordCount.class);
         wordCountJob.setMapperClass(WCMapper.class);
-        if (args.length > 2) {
+        if (conf.getBoolean("wordcount.runcombiner", false)) {
             wordCountJob.setCombinerClass(WCReducer.class);
         }
         wordCountJob.setPartitionerClass(LexicalPartitioner.class);
@@ -28,5 +34,6 @@ public class MyWordCount {
         FileOutputFormat.setOutputPath(wordCountJob, new Path(args[1]));
         wordCountJob.setNumReduceTasks(2);
         wordCountJob.waitForCompletion(true);
+        return 0;
     }
 }
